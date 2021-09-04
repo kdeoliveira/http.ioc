09de60c00b2ce04ff51b34bd92399619d91be9b4
Kevin de Oliveira <contact@kevindeoliveira.com>
2021-09-04 14:45:58 -0400

    travis deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 # https://docs.travis-ci.com/user/environment-variables/#Default-Environment-Variables
+ls
 npm run build
 git remote add url https://${GIT}@github.com/kdeoliveira/kdeoliveira.ioc
 git checkout $TRAVIS_BRANCH
 git add .
 git commit -m "$TRAVIS_COMMIT_MESSAGE"
-git push origin $TRAVIS_BRANCH
+git push url $TRAVIS_BRANCH
