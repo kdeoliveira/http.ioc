d9288cea3d9b719254ca51454724b11f84bb3213
Kevin de Oliveira <contact@kevindeoliveira.com>
2021-09-04 14:38:19 -0400

    travis deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
+# https://docs.travis-ci.com/user/environment-variables/#Default-Environment-Variables
 npm run build
+git checkout -b $TRAVIS_BRANCH
 git add .
-git commit -m "$TRAVIS_COMMIT"
-git push
+git commit -m "$TRAVIS_COMMIT_MESSAGE"
+git push origin $TRAVIS_BRANCH
